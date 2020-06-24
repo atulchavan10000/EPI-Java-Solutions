069df2b74fcb6f882a908fd288ab6b86bac2cdd8
Atul <atulchavan528@gmail.com>
2020-06-24 18:09:27 +0530

    IsTreeSymmetric > pgNo - 128

--- epi/IsTreeSymmetric.java
+++ epi/IsTreeSymmetric.java
@@ -6,7 +6,19 @@ public class IsTreeSymmetric {
 
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
     // TODO - you fill in here.
-    return true;
+    return tree == null || checkSymmetric(tree.left, tree.right);
+  }
+  
+  public static boolean checkSymmetric(BinaryTreeNode<Integer> tree0, BinaryTreeNode<Integer> tree1) {
+	  if(tree0 == null && tree1 == null ) {
+		  return true;
+	  }else if(tree0 != null && tree1 != null) {
+		  return  tree0.data == tree1.data
+			&& checkSymmetric(tree0.left, tree1.right)
+			&& checkSymmetric(tree0.right, tree1.left);
+	  }
+	  // either one of the substrees are not null
+	  return false;
   }
 
   public static void main(String[] args) {
