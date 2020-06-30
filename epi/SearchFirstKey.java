f62ea565e73c3360043f99935f6ca77e926f4005
Atul <atulchavan528@gmail.com>
2020-06-30 20:31:52 +0530

    Search a sorted array for first occurence of K > pgNo-160

--- epi/SearchFirstKey.java
+++ epi/SearchFirstKey.java
@@ -8,7 +8,25 @@ public class SearchFirstKey {
 
   public static int searchFirstOfK(List<Integer> A, int k) {
     // TODO - you fill in here.
-    return 0;
+	  int left = 0;
+	  int right = A.size() - 1;
+	  int result = - 1;
+	  while(left <= right) {
+		  int mid = left + (right - left) / 2;
+		  if(A.get(mid) > k) {
+			  right = mid - 1;
+		  }else if(A.get(mid) == k) {
+			  result = mid;
+			  // nothing to the right can be the first occurence of k
+			  // and we dont want to stop at finding the k, we will stop
+			  // when we find the first occurence, hence next step
+			  right = mid - 1;
+		  }else { // here A.get(mid) < k
+			  left = mid + 1;
+		  }
+	  }
+	  
+    return result;
   }
 
   public static void main(String[] args) {
