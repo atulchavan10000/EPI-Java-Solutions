536800c4ddd256a254885234571f5a37a9f3a1eb
Atul <atulchavan528@gmail.com>
2020-07-17 20:38:18 +0530

    RomanToInteger pgNo->83

--- epi/RomanToInteger.java
+++ epi/RomanToInteger.java
@@ -1,4 +1,6 @@
 package epi;
+import java.util.Map;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class RomanToInteger {
@@ -6,7 +8,25 @@ public class RomanToInteger {
 
   public static int romanToInteger(String s) {
     // TODO - you fill in here.
-    return 0;
+	Map<Character, Integer> map = Map.of(
+			'I', 1,
+			'V', 5,
+			'X', 10,
+			'L', 50,
+			'C', 100,
+			'D', 500,
+			'M', 1000
+			);
+	int sum = map.get(s.charAt(s.length() - 1));
+	for(int i = s.length() - 2; i >= 0; i--) {
+		if(map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+			sum = sum - map.get(s.charAt(i));
+		}else {
+			sum = sum + map.get(s.charAt(i));
+		}
+	}
+	
+    return sum;
   }
 
   public static void main(String[] args) {
