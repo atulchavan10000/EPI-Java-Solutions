c5ad2ad7037bc94a75381aede0bf79441d16743a
Atul <atulchavan528@gmail.com>
2020-07-22 19:31:58 +0530

    LowestCommonAncestorWithParent - pgNO->130

--- epi/LowestCommonAncestorWithParent.java
+++ epi/LowestCommonAncestorWithParent.java
@@ -9,8 +9,41 @@ public class LowestCommonAncestorWithParent {
   public static BinaryTree<Integer> lca(BinaryTree<Integer> node0,
                                         BinaryTree<Integer> node1) {
     // TODO - you fill in here.
-    return null;
-  }
+		int depth0 = getDepth(node0), depth1 = getDepth(node1);
+		
+		// make node0 a deeper node, to simplify the code
+		if(depth0 < depth1) {
+			BinaryTree<Integer> temp = node0;
+			node0 = node1;
+			node1 = temp;
+		}
+		
+		// Ascends from the deeper node
+		int depthDiff = Math.abs(depth0 - depth1);
+		while(depthDiff-- > 0) {
+			node0 = node0.parent;
+		}
+		
+		// Now ascend both nodes until both nodes reach the LCA
+		while(node0 != node1) {
+			node0 = node0.parent;
+			node1 = node1.parent;
+		}
+		
+	    return node0;
+	  }
+	  
+	  public static int getDepth(BinaryTree<Integer> node) {
+		  int depth = 0;
+		  while(node.parent != null) {
+			  depth++;
+
+			  node = node.parent;
+		  }
+		  return depth;
+	  }
+	  
+	  
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
                                Integer key0, Integer key1) throws Exception {
