13ba8f9055574da69b9a6053fea9eb6208c64154
Atul <atulchavan528@gmail.com>
2021-01-28 21:38:18 +0530

    MinimumWaitingTime with greedy approach

--- epi/MinimumWaitingTime.java
+++ epi/MinimumWaitingTime.java
@@ -2,13 +2,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class MinimumWaitingTime {
   @EpiTest(testDataFile = "minimum_waiting_time.tsv")
 
   public static int minimumTotalWaitingTime(List<Integer> serviceTimes) {
     // TODO - you fill in here.
-    return 0;
+	Collections.sort(serviceTimes);
+	int totalWaitingTime = 0;
+	for(int i = 0; i < serviceTimes.size(); i++) {
+		int duration = serviceTimes.get(i);
+		int numRemainingServices = serviceTimes.size() - (i+1);
+		totalWaitingTime += duration*numRemainingServices;
+	}
+    return totalWaitingTime;
   }
 
   public static void main(String[] args) {
