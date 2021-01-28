ca50d396df038a229f5b93061215ed555c07bdc9
Atul <atulchavan528@gmail.com>
2021-01-28 22:56:50 +0530

    Task Pairing - Greedy approach

--- epi/TaskPairing.java
+++ epi/TaskPairing.java
@@ -3,6 +3,8 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class TaskPairing {
   @EpiUserType(ctorParams = {Integer.class, Integer.class})
@@ -41,7 +43,17 @@ public class TaskPairing {
   public static List<PairedTasks>
   optimumTaskAssignment(List<Integer> taskDurations) {
     // TODO - you fill in here.
-    return null;
+	Collections.sort(taskDurations);
+	List<PairedTasks> optimumAssignments = new ArrayList<>();
+	
+	for(int i = 0, j = taskDurations.size() - 1; i < j; i++, j--) {
+		optimumAssignments.add(
+				new PairedTasks(taskDurations.get(i), taskDurations.get(j))
+				);		
+	}
+	
+
+    return optimumAssignments;
   }
 
   public static void main(String[] args) {
